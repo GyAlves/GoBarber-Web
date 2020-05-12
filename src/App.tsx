04c3dd24bc4cd7088e708020cce5a7cc8912d580
Gyasmin <gyasminassuncao@uni9.edu.br>
2020-05-12 02:20:32 -0300

    Login pelo contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/Signin';
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './contexts/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
-    <GlobalStyle />
+    <AuthProvider>
+      <SignIn />
+      <GlobalStyle />
+    </AuthProvider>
   </>
 );
 export default App;
